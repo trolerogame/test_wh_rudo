e0da251b407819f3d7caedbf994c6b216247761d
trolerogame <danielgcampos.04@gmail.com>
2024-09-14 05:23:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require('express')()
+const PORT = 3220
+
+app.post("/rappi/webhook", function (_req,_res) {
+    console.log(_req.body)
+    res.send("webhook of rappi")
+})
+
+app.post("/uber/webhook", function (_req,_res) {
+    console.log(_req.body)
+    res.send("webhook of uber")
+})
+
+app.post("/didi/webhook", function (_req,_res) {
+    console.log(_req.body)
+    res.send("webhook of didi")
+})
+
+app.post("/okana/webhook", function (_req,_res) {
+    console.log(_req.body)
+    res.send("webhook of okana")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`server listening in port:${PORT}`)
+})
